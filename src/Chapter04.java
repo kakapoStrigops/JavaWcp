bc286896a888fe82ac3865b8d26f3be563e97179
kakapoStrigops <emi.yamada@yamadaeminoMacBook-Air.local>
2022-10-24 16:29:01 +0900

    [finish]chap.4_exercise

---
+++ src/Chapter04.java
@@ -0,0 +1,16 @@
+
+public class Chapter04 {
+
+	public static void main(String[] args) {
+
+		String name = "Smith";
+		System.out.println(name);
+
+		name = "Suzuki";
+		System.out.println(name);
+
+		final int CONST = 7;
+		System.out.println(CONST);
+
+	}
+}
