1de357bc2760ae6abef64e9dad9411ea1188204e
kakapoStrigops <emi.yamada@yamadaeminoMacBook-Air.local>
2022-10-24 15:59:06 +0900

    [add]chap.4_constant

---
+++ src/Variable02.java
@@ -0,0 +1,12 @@
+
+public class Variable02 {
+
+	public static void main(String[] argd) {
+		final int MIN_NUMBER = 10;
+		System.out.println(MIN_NUMBER);
+
+		//再代入しようとするとエラーとなるため、コメントアウト
+		//MIN_NUMBER = 20;
+		System.out.println(MIN_NUMBER);
+	}
+}
