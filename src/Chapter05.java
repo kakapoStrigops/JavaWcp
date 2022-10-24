9bde1a0712426180b03223f9a44847d7b3b11c69
kakapoStrigops <emi.yamada@yamadaeminoMacBook-Air.local>
2022-10-24 18:36:47 +0900

    [finish]chap.5_exercise

---
+++ src/Chapter05.java
@@ -0,0 +1,29 @@
+
+public class Chapter05 {
+
+	public static void main(String[] args) {
+
+		long num1 = -9223372036854775808L;
+		double num2 = 123.1234567890123456d;
+		boolean bool1 = true;
+		boolean bool2 = false;
+
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(bool1);
+		System.out.println(bool2);
+
+		Float f = 12.1234565f;
+		System.out.println(f);
+		Integer i = 333;
+		System.out.println(i);
+		Character c = 'を';
+		System.out.println(c);
+
+
+		int[] array1 = {0, 1, 2, 3, 4};
+		System.out.println(array1[3]);
+
+	}
+
+}
